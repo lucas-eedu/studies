5186bee007278a67a27f8d1af76b5f22adce4c9b
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-24 21:57:35 -0300

    Conhendo a modularização do node

---
+++ node/formacao-node-udemy/modulo-1/app.js
@@ -0,0 +1,7 @@
+var calculator = require('./calculator')
+
+console.log(calculator.name)
+console.log(calculator.sum(30,20))
+console.log(calculator.subtraction(30,20))
+console.log(calculator.multiplication(30,20))
+console.log(calculator.division(30,20))
---
+++ node/formacao-node-udemy/modulo-1/calculator.js
@@ -0,0 +1,25 @@
+var name = "Minha calculadora"
+
+function sum(a, b) {
+   return a + b
+}
+
+function subtraction(a, b) {
+   return a - b
+}
+
+function multiplication(a, b) {
+   return a * b
+}
+
+function division(a, b) {
+   return a / b
+}
+
+module.exports = {
+   sum,
+   subtraction,
+   multiplication,
+   division,
+   name
+}
